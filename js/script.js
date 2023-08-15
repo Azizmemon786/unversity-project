876002afd6b6c22f01326b0767803ce0c544c52c
Azizmemon786 <136925916+Azizmemon786@users.noreply.github.com>
2023-08-15 01:10:32 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+
+// var navlist = document.getElementById("navlist");
+
+// function showMenu(){
+//     navlist.style.right = "o";
+// }
+// function hideMenu(){
+//     navlist.style.right = "-250px";
+// }
